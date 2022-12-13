92a10eda454245708d9dc2d8e5244d96f46262e9
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2022-12-13 23:40:33 +0530

    var, key,comment

---
+++ comment.py
@@ -0,0 +1,7 @@
+#single line comment in python
+''' hello guys
+i just started learning about python and nearly completed few concepts like
+creating files using idle
+running files from idle and cmd
+learned about comments, variables, type() method provides the data type
+may be find something interesting in coming classes '''
